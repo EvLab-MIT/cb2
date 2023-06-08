4feb43848c854477e8d97e48ab2a9b5b578be6ff
aalok-sathe <asathe@mit.edu>
2023-06-08 12:58:55 -0400

    maybe nail down the import structure

--- drivercb2/driver.py
+++ drivercb2/driver.py
@@ -1,4 +1,8 @@
-from fixation import show_fixation
+from drivercb2.fixation import show_fixation
+from py_client.remote_client import RemoteClient
 
 if __name__ == "__main__":
-    show_fixation(3)
+    show_fixation(1)
+
+    url = "0.0.0.0:8000"
+    rc = RemoteClient(url=url, lobby_name="scenario-lobby")
