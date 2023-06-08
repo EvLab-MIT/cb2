912ce5c1ab06f508ef6d02da18b162a8b68e1d86
aalok-sathe <asathe@mit.edu>
2023-06-08 13:16:47 -0400

    try to connect the client

--- drivercb2/driver.py
+++ drivercb2/driver.py
@@ -4,5 +4,8 @@ from py_client.remote_client import RemoteClient
 if __name__ == "__main__":
     show_fixation(1)
 
-    url = "0.0.0.0:8000"
-    rc = RemoteClient(url=url, lobby_name="scenario-lobby")
+    url = "0.0.0.0:8080"
+    client = RemoteClient(url=url, lobby_name="scenario-lobby")
+
+    connected, reason = client.Connect()
+    assert connected, f"Unable to connect: {reason}"
